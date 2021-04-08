def5c9038ca9718b32240226558aa630876afcaf
echils <echils@outlook.com>
2021-04-08 22:42:56 +0800

    add maven support

---
+++ src/main/java/com/github/api/ApiDocumentConfiguration.java
@@ -0,0 +1,17 @@
+package com.github.api;
+
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * This configuration class is used to manage api document
+ * parsing components in the spring container
+ *
+ * @author echils
+ * @since 2021-02-25 21:34:31
+ */
+@Configuration
+public class ApiDocumentConfiguration {
+
+
+}
---
+++ src/main/java/com/github/api/EnableApiReporterDelegate.java
@@ -0,0 +1,20 @@
+package com.github.api;
+
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+/**
+ * EnableApiReporterDelegate
+ * Decorate the startup class with this annotation to enable automatic hosting of API documents
+ *
+ * @author echils
+ * @since 2020-12-01 16:06
+ */
+@Target(value = ElementType.TYPE)
+@Documented
+@Retention(value = RetentionPolicy.RUNTIME)
+@Import(ApiDocumentConfiguration.class)
+public @interface EnableApiReporterDelegate {
+
+}
