56554a6724ccdf2a9fa908e71c5f08e2caad416c
echils <echils@outlook.com>
2021-05-12 17:52:18 +0800

    Interface development progress 41%

--- src/main/java/com/github/api/core/refer/Documentation.java
+++ src/main/java/com/github/api/core/refer/Documentation.java
@@ -1,5 +1,6 @@
 package com.github.api.core.refer;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -33,6 +34,8 @@ public class Documentation {
         }
         try {
             ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+            objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
             return objectMapper.writer(new DefaultPrettyPrinter()).writeValueAsString(swagger);
         } catch (JsonProcessingException e) {
             throw new RuntimeException("Could not write YAML", e);
@@ -49,6 +52,8 @@ public class Documentation {
         }
         try {
             ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+            objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
             return objectMapper.writeValueAsString(swagger);
         } catch (JsonProcessingException e) {
             throw new RuntimeException("Could not write JSON", e);
